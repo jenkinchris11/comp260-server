d78b4a23c5b6f60193aa49ac33bb8b2ea9eb92c3
jenkinchris11 <jenkinchris11@gmail.com>
2019-03-01 15:39:38 +0000

    Add files via upload

--- Client/client.py
+++ Client/client.py
@@ -1,16 +1,45 @@
 import socket
+import time
+
 if __name__ == '__main__':
-    mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    mySocket.connect(("127.0.0.1", 8222))
+    isConnected = False
+    mySocket = None
 
-    testString = "this is a test from the python client"
+    isRunning = True
 
-    mySocket.send(testString.encode())
+    while isRunning == True:
+        while isConnected == False:
 
-    while True:
-        data = mySocket.recv(4096)
-        print(data.decode("utf-8"))
+            if mySocket is None:
+                mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+            try:
+                mySocket.connect(("127.0.0.1", 8222))
+                isConnected = True
+            except socket.error:
+                isConnected = False
+
+            if(isConnected == True):
+                try:
+                    testString = "this is a test from the python client"
+                    mySocket.send(testString.encode())
+                except:
+                    isConnected = False
+                    mySocket = None
+
+                    if isConnected == False:
+                        print("No server")
+                        time.sleep(1.0)
+
+
+        while isConnected == True:
+            try:
+                data = mySocket.recv(4096)
+                print(data.decode("utf-8"))
+            except:
+                isConnected = False
+                mySocket = None
 
 
 
