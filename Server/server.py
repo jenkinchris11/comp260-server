925801b35184be513b89a0128a7441b52f4df767
jenkinchris11 <jenkinchris11@gmail.com>
2019-03-01 15:40:01 +0000

    Server

--- Server/server.py
+++ Server/server.py
@@ -2,28 +2,42 @@ import socket
 import time
 
 if __name__ == '__main__':
-    mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    mySocket.bind(("127.0.0.1", 8222))
-    mySocket.listen(5)
-
-    client = mySocket.accept()
-
-    data = client[0].recv(4096)
-
-    print(data.decode("utf-8"))
+    isRunning = True
+    isConnected = False
 
     seqID = 0
 
-    while True:
-        testString = str(seqID) +":" + time.ctime()
-
-        client[0].send(testString.encode())
-
-        seqID+=1
-        time.sleep(0.5)
+    mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    mySocket.bind(("127.0.0.1", 8222))
+    mySocket.listen(5)
 
 
+    while isRunning == True:
+        if isConnected == False:
+            print("Waiting for client ...")
+            client = mySocket.accept()
 
+        try:
+            data = client[0].recv(4096)
+            print(data.decode("utf-8"))
+            seqID = 0
+            isConnected = True
+            print("Client connected")
+        except socket.error:
+            isConnected = False
+
+        while isConnected == True:
+            testString = str(seqID) +":" + time.ctime()
+
+            try:
+                print("Sending test string: " + testString)
+                client[0].send(testString.encode())
+                seqID += 1
+                time.sleep(0.5)
+            except socket.error:
+                isConnected = False
+                client = None
+                print("Client lost")
 
 
